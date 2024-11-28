d1d4abaa20656b20a651cfe5b054c56d3f70bf92
Cristian Berrio <cristianberrio9864@gmail.com>
2024-11-28 08:47:10 -0500

    Generate report

--- models/account_balance_sheet.py
+++ models/account_balance_sheet.py
@@ -19,3 +19,36 @@ class AccountBalanceSheet(models.Model):
     partner_id = fields.Many2one('res.partner', string='Partner')
     account_id_from = fields.Many2one('account.account', string='From Account')
     account_id_to = fields.Many2one('account.account', string='To Account')
+
+    def generate_report(self):
+        if not self.date_from or not self.date_to:
+            raise UserError("Debes completar las fechas 'Desde' y 'Hasta'.")
+
+        date_from = self.date_from.strftime("%Y-%m-%d")
+        date_to = self.date_to.strftime("%Y-%m-%d")
+
+        partner_domain = [('date', '>=', date_from), ('date', '<=', date_to)]
+        if self.account_id_from:
+            partner_domain.append(('account_id.code', '>=', self.account_id_from.code))
+        if self.account_id_to:
+            partner_domain.append(('account_id.code', '<=', self.account_id_to.code))
+        if self.partner_id:
+            partner_domain.append(('partner_id', '=', self.partner_id.id))
+
+        self.env['account.balance_sheet'].search([]).unlink()
+        excluded_codes = []
+        partner_domain.append(('account_id.code', 'not in', excluded_codes))
+
+        moves = self.env['account.move.line'].search(partner_domain)
+
+        for move in moves:
+            self.env['account.balance_sheet'].create({
+                'account': f"{move.account_id.name}",
+                'code_digits': move.account_id.code,
+                'user': move.partner_id.name,
+                'company_doc': move.partner_id.vat,
+                'start_balance': move.cumulated_balance,
+                'debit': move.debit,
+                'credit': move.credit,
+                'final_balance': move.balance,
+            })
